a5c338095fa3c150de6ef2647969ae84a733bdda
Matt Mohan <matt.mohan@wishabi.com>
2025-12-02 10:08:43 -0500

    Add day 2 solution

--- day2/day2.go
+++ day2/day2.go
@@ -1,6 +1,9 @@
 package day2
 
 import (
+	"strconv"
+	"strings"
+
 	"git.mattmohan.com/matt/advent2025/days"
 )
 
@@ -9,16 +12,102 @@ func GetDay() days.Day {
 		Number: 2,
 		Name:   "Sample Day",
 		Parts: [2]days.DayPart{
-			{PartFunc: func(input []byte, result chan string, progress chan float64) {
-				defer close(result)
-				// Placeholder logic for Part A
-				result <- "Not implemented yet"
-			}},
-			{PartFunc: func(input []byte, result chan string, progress chan float64) {
-				defer close(result)
-				// Placeholder logic for Part B
-				result <- "Not implemented yet"
-			}},
+			{PartFunc: day2Part1},
+			{PartFunc: day2Part2},
 		},
 	}
+}
+
+type Range struct {
+	start int
+	end   int
+}
+
+func parseInput(inputChars []byte) []Range {
+	rangeStrings := strings.Split(string(inputChars), ",")
+	ranges := make([]Range, 0, len(rangeStrings))
+
+	for _, rangeString := range rangeStrings {
+		trimmedRange := strings.TrimSpace(rangeString)
+		parts := strings.Split(trimmedRange, "-")
+		start, err := strconv.Atoi(parts[0])
+		if err != nil {
+			panic(err)
+		}
+		end, err := strconv.Atoi(parts[1])
+		if err != nil {
+			panic(err)
+		}
+		ranges = append(ranges, Range{start: start, end: end})
+	}
+	return ranges
+}
+
+func day2Part1(inputChars []byte, result chan string, progress chan float64) {
+	defer close(result)
+	defer close(progress)
+
+	count := 0
+	ranges := parseInput(inputChars)
+
+	for _, currentRange := range ranges {
+	rangeloop:
+		for i := currentRange.start; i <= currentRange.end; i++ {
+			str := strconv.Itoa(i)
+			if len(str)%2 != 0 {
+				continue
+			}
+			midpoint := len(str) / 2
+			firstHalf := str[:midpoint]
+			secondHalf := str[midpoint:]
+			for j := 0; j < midpoint; j++ {
+				if firstHalf[j] != secondHalf[j] {
+					continue rangeloop
+				}
+			}
+			count += i
+		}
+	}
+	// Placeholder logic for Part A
+	result <- strconv.Itoa(count)
+}
+
+func day2Part2(inputChars []byte, result chan string, progress chan float64) {
+	defer close(result)
+	defer close(progress)
+
+	count := 0
+	ranges := parseInput(inputChars)
+
+	// Iterate over all of the ranges
+	for rangeIdx, currentRange := range ranges {
+		// Iterate over all numbers in the range
+		for i := currentRange.start; i <= currentRange.end; i++ {
+			// For each number, try skipping digits to find a repeat pattern starting with a skip distance of 1 up to half the length of the number
+			str := strconv.Itoa(i)
+			longestSkip := len(str) / 2
+
+		skiploop:
+			for skip := 1; skip <= longestSkip; skip++ {
+				// Bail if not divisible
+				if len(str)%skip != 0 {
+					continue skiploop
+				}
+
+				// Start at each position and check that everything within the window matches
+				for startCheck := 0; startCheck < len(str)-skip; startCheck++ {
+					if str[startCheck] != str[(startCheck+skip)] {
+						continue skiploop
+					}
+				}
+				// If we got here, we found a valid skip so accumulate and move on
+				count += i
+				break skiploop
+			}
+		}
+		// Report back on progress
+		progress <- float64(rangeIdx) / float64(len(ranges))
+	}
+	// Placeholder logic for Part A
+	result <- strconv.Itoa(count)
 }
---
+++ day2/day2_test.go
@@ -0,0 +1,45 @@
+package day2
+
+import (
+	"testing"
+)
+
+func TestSolveDay2PartA(t *testing.T) {
+	testInput := []byte(`11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124`)
+	expectedOutput := "1227775554"
+	progress := make(chan float64, 100)
+	result := make(chan string)
+	go func() {
+		day2Part1(testInput, result, progress)
+	}()
+	output := <-result
+
+	if output != expectedOutput {
+		t.Errorf("Expected %s, got %s for Solve", expectedOutput, output)
+	}
+	for range progress {
+
+	}
+}
+
+func TestSolveDay2PartB(t *testing.T) {
+	testInput := []byte(`11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124`)
+	expectedOutput := "4174379265"
+	progress := make(chan float64, 100)
+	result := make(chan string)
+	go func() {
+		day2Part2(testInput, result, progress)
+	}()
+	output := <-result
+
+	if output != expectedOutput {
+		t.Errorf("Expected %s, got %s for Solve", expectedOutput, output)
+	}
+	for range progress {
+
+	}
+}
