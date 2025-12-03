3c5887f2a94b7404f7afaccae69066f4e70eabc5
Matt Mohan <matt.mohan@wishabi.com>
2025-12-03 09:03:24 -0500

    Imnprove performance of the progress bar updating logic

--- days/days.go
+++ days/days.go
@@ -21,7 +21,7 @@ type Day struct {
 
 func (day *Day) Run(partIdx PartNumber, taskProg chan Progress) tea.Cmd {
 	part := &day.Parts[partIdx]
-	progress := make(chan float64)
+	progress := make(chan float64, 1000)
 	result := make(chan string)
 
 	taskProg <- Progress{Percent: 0.0, Done: false, Day: day.Number - 1, Part: partIdx}
@@ -29,11 +29,14 @@ func (day *Day) Run(partIdx PartNumber, taskProg chan Progress) tea.Cmd {
 	start := time.Now()
 	go part.PartFunc(day.Input, result, progress)
 	done := false
-	nextProgress := float32(0.0)
+	nextProgress := float64(0.0)
 	var timer *time.Timer
+
+	// Loop until the result is ready
 	for !done {
 		select {
 		case res := <-result:
+			// Execution is complete so record final result and duration, stop the progress updates, and push a final progress update to 100%
 			part.Duration = time.Since(start)
 			part.Result = res
 			if timer != nil {
@@ -42,10 +45,12 @@ func (day *Day) Run(partIdx PartNumber, taskProg chan Progress) tea.Cmd {
 			taskProg <- Progress{Percent: float32(1.0), Done: true, Day: day.Number - 1, Part: partIdx}
 			done = true
 		case p := <-progress:
-			nextProgress = float32(p)
+			// Throttle progress updates to at most every 10ms
+			nextProgress = p
 			if timer == nil {
 				timer = time.AfterFunc(10*time.Millisecond, func() {
-					taskProg <- Progress{Percent: nextProgress, Done: false, Day: day.Number - 1, Part: partIdx}
+					// Convert the raw number to a message for the Update loop
+					taskProg <- Progress{Percent: float32(nextProgress), Done: false, Day: day.Number - 1, Part: partIdx}
 					timer = nil
 				})
 			}
