fd50996268855113554bb0fd0eda2555868e415b
Matt Mohan <matt.mohan@wishabi.com>
2025-12-04 11:09:09 -0500

    Add day3 code

--- day3/day3.go
+++ day3/day3.go
@@ -1,24 +1,99 @@
 package day3
 
 import (
+	"strconv"
+	"strings"
+
 	"git.mattmohan.com/matt/advent2025/days"
 )
 
 func GetDay() days.Day {
 	return days.Day{
 		Number: 3,
-		Name:   "Sample Day",
+		Name:   "Lobby",
 		Parts: [2]days.DayPart{
-			{PartFunc: func(input []byte, result chan string, progress chan float64) {
-				defer close(result)
-				// Placeholder logic for Part A
-				result <- "Not implemented yet"
-			}},
-			{PartFunc: func(input []byte, result chan string, progress chan float64) {
-				defer close(result)
-				// Placeholder logic for Part B
-				result <- "Not implemented yet"
-			}},
+			{PartFunc: day3Part1},
+			{PartFunc: day3Part2},
 		},
 	}
+}
+
+func day3Part1(inputChars []byte, result chan string, progress chan float64) {
+	defer close(result)
+	defer close(progress)
+
+	count := 0
+	banks := strings.Split(string(inputChars), "\n")
+	for i, bank := range banks {
+		count += findHighest(convertStringBankToIntSlice(bank), 2)
+		progress <- float64(i) / float64(len(banks))
+	}
+
+	result <- strconv.Itoa(count)
+}
+
+func day3Part2(inputChars []byte, result chan string, progress chan float64) {
+	defer close(result)
+	defer close(progress)
+
+	count := 0
+	banks := strings.Split(string(inputChars), "\n")
+	for i, bank := range banks {
+		count += findHighest(convertStringBankToIntSlice(bank), 12)
+		progress <- float64(i) / float64(len(banks))
+	}
+
+	result <- strconv.Itoa(count)
+}
+
+func convertStringBankToIntSlice(bankString string) []int {
+	bank := make([]int, len(bankString))
+	for i := range bankString {
+		val, err := strconv.Atoi(string(bankString[i]))
+		if err != nil {
+			panic(err)
+		}
+		bank[i] = val
+	}
+	return bank
+}
+
+func findHighest(bank []int, length int) int {
+	highIdxs := make([]int, length)
+
+	// Start by assuming the first digits are the highest
+	for i := range highIdxs {
+		highIdxs[i] = i
+	}
+
+	start := 0
+	// For each place in the output find the appropriate highest digit
+	for place := 0; place < length; place++ {
+		// After the first place start searching after the last highest digit found
+		if place > 0 {
+			start = highIdxs[place-1] + 1
+		}
+
+		// Stop searching so that there are enough digits left to fill the remaining places
+		end := len(bank) - (length - place - 1)
+
+		for i := start; i < end; i++ {
+			if bank[i] > bank[highIdxs[place]] {
+				// If found update the highest index for this place
+				highIdxs[place] = i
+				// And update all subsequent places to be one after this
+				for j := place + 1; j < length; j++ {
+					highIdxs[j] = highIdxs[j-1] + 1
+				}
+			}
+		}
+	}
+
+	// Build the final number from the highest digits found
+	sum := 0
+	for _, idx := range highIdxs {
+		sum = sum*10 + bank[idx]
+	}
+
+	return sum
 }
---
+++ day3/day3_test.go
@@ -0,0 +1,50 @@
+package day3
+
+import (
+	"testing"
+)
+
+var textInput = []byte(`987654321111111
+811111111111119
+234234234234278
+818181911112111`)
+
+func TestDay3Part1(t *testing.T) {
+	// Placeholder test for Part 1
+	expected := "357"
+	resultChan := make(chan string)
+	progressChan := make(chan float64)
+	go day3Part1(textInput, resultChan, progressChan)
+	result := <-resultChan
+	if result != expected {
+		t.Errorf("Expected %s, got %s", expected, result)
+	}
+}
+
+func TestDay3Part2(t *testing.T) {
+	// Placeholder test for Part 2
+	table := []struct {
+		input    []byte
+		expected string
+	}{
+		{[]byte(
+			"987654321111111"), "987654321111"},
+		{[]byte(
+			"811111111111119"), "811111111119"},
+		{[]byte("234234234234278"), "434234234278"},
+		{[]byte("818181911112111"), "888911112111"},
+		{[]byte(`987654321111111
+811111111111119
+234234234234278
+818181911112111`), "3121910778619"},
+	}
+	for _, test := range table {
+		resultChan := make(chan string)
+		progressChan := make(chan float64)
+		go day3Part2(test.input, resultChan, progressChan)
+		result := <-resultChan
+		if result != test.expected {
+			t.Errorf("For input: \n===\n%s\n===\n  expected: %s,\n  got:      %s", test.input, test.expected, result)
+		}
+	}
+}
